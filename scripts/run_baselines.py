da2b91f88d066b46a4e07fadc000117a95177e48
Xiaochen Zhu <zhuxiaochen1005@gmail.com>
2022-10-17 14:57:38 +0800

    Only run ldpgcn instead of rr since rr is too dense

--- scripts/run_baselines.py
+++ scripts/run_baselines.py
@@ -46,7 +46,7 @@ linkless_graph.edge_index = None
 
 eps_list = [1,2,3,4,5,6,7,8]
 # for now, don't run solitude because it's real slow (grid search part, two many hparams)
-mechanisms = ["rr", "ldpgcn"]
+mechanisms = ["ldpgcn"]
 run_experiment = {
     "rr": rr.run_rr,
     "ldpgcn": ldpgcn.run_ldpgcn,
